bea3f96ef87c649a64630869654ed917fd9f76f6
andrewasaleh <salehandrew110@gmail.com>
2023-09-23 15:35:43 -0700

    9/23/23 Login Screen UI Update

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -7,31 +7,34 @@ import password_icon from '../Assets/password.png'
 
 export const LoginSignup = () => {
 
+    const [action,setAction] = React.useState("Login");
+
   return (
     <div className='container'>
         <div className="header">
-            <div className="text">Sign Up</div>
+            <div className="text">{action}</div>
             <div className="underline"></div>
         </div>
         <div className="inputs">
-        <div className="input">
+            {action==="Login"?<div></div>:<div className="input">
             <img src={user_icon} alt="" />
-            <input type="text" placeholder="Name" />
-        </div>
+            <input placeholder="Name" type="text" />
+        </div>}
+
         <div className="input">
             <img src={email_icon} alt="" />
-            <input type="email" placeholder="Email Id" />
+            <input placeholder="Email" type="email" />
         </div>
         <div className="input">
             <img src={password_icon} alt="" />
-            <input type="password" placeholder="Password" />
+            <input placeholder="Password" type="password" />
             </div>
         </div>
-        <div className="forgot-password">Forgot Pasword? <span>Click Here!</span></div>
+        {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
+}
         <div className="submit-container">
-            <div className="submit">Sign Up</div>
-            <div className="submit">Login</div>
-
+            <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
         </div>       
     </div>
   )
